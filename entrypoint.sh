51cb7fab0a1a67e5040c22741d6b1c85786ae579
Ehsan1988dproton <Ehsan1988d@proton.me>
2026-09-13 19:32:42 +0330

    Enhance entrypoint.sh for Node Certificate setup
    Updated entrypoint.sh to set up Node Certificate and start Node Daemon.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-echo "[Info] Rebecca Node container is running..."
-# نگه داشتن کانتینر در حالت فعال برای بررسی و عیب‌یابی
-tail -f /dev/null
+
+echo "[Info] Setting up Node Certificate..."
+mkdir -p /var/lib/marzban-node
+
+# قرار دادن گواهی و کلید در مسیر استاندارد نود
+if [ -n "$NODE_CERT" ]; then
+    echo "$NODE_CERT" > /var/lib/marzban-node/ssl_client_cert.pem
+fi
+
+if [ -n "$NODE_KEY" ]; then
+    echo "$NODE_KEY" >> /var/lib/marzban-node/ssl_client_cert.pem
+fi
+
+echo "[Info] Starting Node Daemon..."
+exec python3 main.py
